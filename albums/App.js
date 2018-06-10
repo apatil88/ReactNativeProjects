d7f31338985b95bb137fcbc47c949229c8f4bc03
apatil88 <amrut.patil88@hotmail.com>
2018-06-10 19:44:43 -0400

    Adding styling to Header Component

--- albums/App.js
+++ albums/App.js
@@ -5,37 +5,23 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/header";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <View style={styles.viewStyle}>
+        <Header />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  viewStyle: {
+    backgroundColor: "#F8F8F8"
   }
 });
+
+export default App;
--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -3,11 +3,14 @@ import { Text, StyleSheet } from "react-native";
 
 class Header extends Component {
   render() {
-    return <Text style={styles.textStyle}>Albums </Text>;
+    return <Text style={styles.textStyle}>Albums</Text>;
   }
 }
 
 const styles = StyleSheet.create({
+  viewStyle: {
+    backgroundColor: "#F8F8F8"
+  },
   textStyle: {
     fontSize: 20
   }
